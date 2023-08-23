4aa2709b18dbb7ecd398498094cf9b70e4692305
Francis <francis.francis20.1989@gmail.com>
2023-08-23 12:06:29 +0530

    vijay

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JsonService } from '../json.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  jsonData: any;
+  constructor(private jsonService: JsonService) {}
+  ngOnInit() {
+    this.jsonService.getJsonData().subscribe(data => {
+      this.jsonData = data;
+      console.log(this.jsonData);
+    });
+  }
+  videoId: string = 'v=2AnaCDISHPs';
 
 }
